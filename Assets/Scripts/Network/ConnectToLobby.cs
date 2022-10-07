a5a01ba7118c2d63fc0778a61b807f0cc550f45a
TSentler <centner_dc@mail.ru>
2022-10-07 10:12:54 +0500

    Dropdown room list

---
+++ Assets/Scripts/Network/ConnectToLobby.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Network
+{
+    public class ConnectToLobby : MonoBehaviour
+    {
+        private IEnumerator Start()
+        {
+            yield break;
+        }
+    }
+}
--- Assets/Scripts/Network/ConnectToServer.cs
+++ Assets/Scripts/Network/ConnectToServer.cs
@@ -1,18 +1,23 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
 using UnityEngine;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
     [SerializeField] private GameObject _witPanel;
-    [SerializeField] private TMP_InputField _create, _join;
+    [SerializeField] private TMP_InputField _create;
+    [SerializeField] private TMP_Dropdown _join;
+    [SerializeField] private TMP_Text _joinLabel;
 
     private void Awake()
     {
         _witPanel.SetActive(true);
         _create.text = "Room1";
-        _join.text = "Room1";
+        _join.ClearOptions();
     }
 
     private IEnumerator Start()
@@ -23,21 +28,47 @@ public class ConnectToServer : MonoBehaviourPunCallbacks
             yield return new WaitForSeconds(0.5f);
         }
         _witPanel.SetActive(false);
+        while (PhotonNetwork.InLobby == false)
+        {
+            PhotonNetwork.JoinLobby(TypedLobby.Default);
+            yield return new WaitForSeconds(2f);
+        }
     }
 
     public void CreateRoom()
     {
-        Debug.Log(_create.text);
         PhotonNetwork.CreateRoom(_create.text);
     }
 
     public void JoinRoom()
     {
-        PhotonNetwork.JoinRoom(_join.text);
+        PhotonNetwork.JoinRoom(_join.options[_join.value].text);
     }
 
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("OnJoinedLobby");
+    }
+    
     public override void OnJoinedRoom()
     {
         PhotonNetwork.LoadLevel("Game");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        var roomNames = (from room in roomList
+            where room.IsVisible && room.IsOpen
+            orderby room.Name
+            select room.Name).ToList();
+        _join.ClearOptions();
+        if (roomNames.Count == 0)
+        {
+            _joinLabel.SetText("Search Rooms..");
+        }
+        else
+        {
+            _join.AddOptions(roomNames);
+        }
     }
 }
