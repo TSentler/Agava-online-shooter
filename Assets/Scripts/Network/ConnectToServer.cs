235299cba9c62200decfd458a2b42d1ead563c14
TSentler <centner_dc@mail.ru>
2022-10-13 14:22:15 +0500

    Add MatchmakingCallbacksCatche

--- Assets/Scripts/Network/ConnectToServer.cs
+++ Assets/Scripts/Network/ConnectToServer.cs
@@ -5,15 +5,35 @@ using UnityEngine.Events;
 
 namespace Network
 {
-    public class ConnectToServer : MonoBehaviourPunCallbacks
+    public class ConnectToServer : MonoBehaviour
     {
+        private readonly string _defaultSceneName = "Room1";
+        
         private Coroutine _connectCoroutine;
-
+        private ConnectionCallbackCatcher _connectCallback;
+        private MatchmakingCallbacksCatcher _matchCallback;
+        
         public event UnityAction OnConnectStart, OnConnectEnd, OnDisconnect;
         
         private void Awake()
         {
             PhotonNetwork.AutomaticallySyncScene = true;
+            _connectCallback = FindObjectOfType<ConnectionCallbackCatcher>();
+            _matchCallback = FindObjectOfType<MatchmakingCallbacksCatcher>();
+        }
+
+        private void OnEnable()
+        {
+            _connectCallback.OnConnectToMaster += ConnectedToMasterHandler;
+            _connectCallback.OnDisconnnect += DisconnectHandler;
+            _matchCallback.OnJoinRoom += JoinRoomHandler;
+        }
+
+        private void OnDisable()
+        {
+            _connectCallback.OnConnectToMaster -= ConnectedToMasterHandler;
+            _connectCallback.OnDisconnnect -= DisconnectHandler;
+            _matchCallback.OnJoinRoom -= JoinRoomHandler;
         }
 
         private void Start()
@@ -31,6 +51,23 @@ namespace Network
             PhotonNetwork.ConnectUsingSettings();
         }
 
+        private void ConnectedToMasterHandler()
+        {
+            OnConnectEnd?.Invoke();
+        }
+
+        private void DisconnectHandler(DisconnectCause cause)
+        {
+            Debug.Log("Disconnect");
+            OnDisconnect?.Invoke();
+            Connect();
+        }
+        
+        private void JoinRoomHandler()
+        {
+            PhotonNetwork.LoadLevel(_defaultSceneName);
+        }
+        
         public void CreateOrJoin()
         {
             if (PhotonNetwork.IsConnectedAndReady)
@@ -47,23 +84,6 @@ namespace Network
         public void JoinRoom(string name)
         {
             PhotonNetwork.JoinRoom(name);
-        }
-
-        public override void OnJoinedRoom()
-        {
-            PhotonNetwork.LoadLevel("Room1");
-        }
-        
-        public override void OnConnectedToMaster()
-        {
-            OnConnectEnd?.Invoke();
-        }
-
-        public override void OnDisconnected(DisconnectCause cause)
-        {
-            Debug.Log("Disconnect");
-            OnDisconnect?.Invoke();
-            Connect();
         }
     }
 }
