1897d894649c0c270e132838c93ecb2a5b6f78d7
TSentler <centner_dc@mail.ru>
2022-10-17 11:51:10 +0500

    Increase input axis gravity

--- Assets/Scripts/PlayerAbilities/PlayerMovement.cs
+++ Assets/Scripts/PlayerAbilities/PlayerMovement.cs
@@ -3,50 +3,48 @@ using UnityEngine;
 
 namespace PlayerAbilities
 {
+    [RequireComponent(typeof(PhotonView),
+        typeof(Animator),
+        typeof(CharacterController))]
     public class PlayerMovement : MonoBehaviour 
     {
         private const string RunAnimation = "IsRun";
     
         private Animator _animator;
+        private PhotonView _photonView;
+        private CharacterController _characterController;
     
         [SerializeField] private float _speed;
-        [SerializeField] private PhotonView _photonViev;
-        [SerializeField] private CharacterController _characterController;
 
         private void Awake()
         {
             _animator = GetComponent<Animator>();
-            _photonViev.TransferOwnership(PhotonNetwork.LocalPlayer);
+            _characterController = GetComponent<CharacterController>();
+            _photonView = GetComponent<PhotonView>();
+            _photonView.TransferOwnership(PhotonNetwork.LocalPlayer);
         }
 
         private void Update()
         {
-            if (_photonViev.IsMine == false)
+            if (_photonView.IsMine == false)
                 return;
-        
-            float horizontalInput = Input.GetAxis ("Horizontal");
-            float verticalInput = Input.GetAxis ("Vertical");
+            
+            var direction = GetDirection();
+            var distance = direction * _speed * Time.deltaTime;
+            _characterController.Move(distance);
+            var isRun = direction != Vector3.zero;
+            _animator.SetBool(RunAnimation, isRun);
+        }
+
+        private Vector3 GetDirection()
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
 
             Vector3 moveDirectionForward = transform.forward * verticalInput;
             Vector3 moveDirectionSide = transform.right * horizontalInput;
 
-            Vector3 direction = (moveDirectionForward + moveDirectionSide).normalized;
-            // Vector3 inverseDirection = transform.InverseTransformDirection(direction);
-            Vector3 distance = direction * _speed * Time.deltaTime;
-            if (direction != Vector3.zero)
-            {
-                _characterController.Move(distance);
-                _animator.SetBool(RunAnimation, true);
-            }
-            else
-            {
-                _animator.SetBool(RunAnimation, false);
-            }
-        }
-
-        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-        {
-        
+            return (moveDirectionForward + moveDirectionSide).normalized;
         }
     }
 }
