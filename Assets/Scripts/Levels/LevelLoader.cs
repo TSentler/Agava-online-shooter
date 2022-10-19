323c8c98c6bad7b03f04f4ee92ae85abec19f8b2
Daehico <empire_95@mail.ru>
2022-10-19 20:42:18 +0300

    +

--- Assets/Scripts/Levels/LevelLoader.cs
+++ Assets/Scripts/Levels/LevelLoader.cs
@@ -25,6 +25,7 @@ namespace Levels
             _matchCallback = FindObjectOfType<MatchmakingCallbacksCatcher>();
             _roomOptions = new RoomOptions();
             _roomOptions.MaxPlayers = _maxPlayersCount;
+            _roomOptions.CleanupCacheOnLeave = true;
         }
 
         private void OnEnable()
