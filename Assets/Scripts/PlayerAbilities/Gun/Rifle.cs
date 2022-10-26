dceebc3793264ccc7e001e6a4c39e3805e6b250e
Shennod <Shennod1995@gmail.com>
2022-10-26 13:58:28 +0300

    code style

--- Assets/Scripts/PlayerAbilities/Gun/Rifle.cs
+++ Assets/Scripts/PlayerAbilities/Gun/Rifle.cs
@@ -1,43 +1,26 @@
 using Photon.Pun;
-using PlayerAbilities;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class Rifle : Gun
 {
-    [SerializeField] private float _damage;
-    [SerializeField] private ParticleSystem _shootParticle;
-    [SerializeField] private float _recoilForceXMax;
-    [SerializeField] private float _recoilForceYMax;
-    [SerializeField] private float _recoilForceXMin;
-    [SerializeField] private float _recoilForceYMin;
-    [SerializeField] private int _maxAmmoCount;
-
-    private int _maxAmmoQuanity;
     private float _accumulatedTime;
     private float _fireaInterval;
-
-    private void Start()
-    {
-        _ammoQuanity = _maxAmmo;
-        _maxAmmoQuanity = _maxAmmoCount;
-    }
 
     private void Update()
     {
         _accumulatedTime += Time.deltaTime;
 
-        if (_canShoot == true)
+        if (CanShoot == true)
+        {
+            CanShoot = false;
             return;
+        }
 
         _fireaInterval += Time.deltaTime;
 
-        if(_fireaInterval >= _delayPerShoot)
+        if (_fireaInterval >= DelayPerShoot)
         {
-            _canShoot = true;
+            CanShoot = true;
             _fireaInterval = 0;
         }
     }
@@ -51,75 +34,15 @@ public class Rifle : Gun
                 if (Input.GetMouseButton(0))
                 {
                     Shoot(Camera);
-                }
-            }
-        }
-    }
 
-    public override void Shoot(Camera camera)
-    {
-        if (_ammoQuanity > 0 && _canShoot)
-        {
-            _canShoot = false;
-            _shootParticle.Play();
-            MouseLook.Shoot(_recoilForceXMin, _recoilForceYMin, _recoilForceXMax, _recoilForceYMax);
-            ShootSound.Play();
-
-            Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
-            ray.origin = camera.transform.position;
-
-            RaycastHit[] hits = Physics.RaycastAll(ray, LayerToDetect);
-
-            RaycastHit minDistanceHit = new RaycastHit
-            {
-                distance = MinDistanceHit
-            };
-
-            for (int i = 0; i < hits.Length; i++)
-            {
-                if (hits[i].collider.gameObject.TryGetComponent(out HitDetector hitDetector))
-                {
-                    if (hitDetector.PhotonView.IsMine == false)
+                    if (MaxAmmoQuanity <= 0)
                     {
-                        hitDetector.DetectHit(_damage, PhotonNetwork.LocalPlayer);
-                        OnHit();
-                    }
-                    break;
-                }
-                else
-                {
-                    for (int j = 0; j < hits.Length; j++)
-                    {
-                        if (hits[j].distance < minDistanceHit.distance)
-                        {
-                            minDistanceHit = hits[j];
-                        }
-                    }
-
-                    if (minDistanceHit.collider.gameObject.TryGetComponent(out PlayerHealth playerHealth) == false)
-                    {
-                        PhotonView.RPC(nameof(ShootRpc), RpcTarget.All, minDistanceHit.point, minDistanceHit.normal);
+                        AmmoQuanity = MaxAmmo;
+                        MaxAmmoQuanity = MaxAmmoCount;
+                        CanShoot = true;
+                        OnEmptyAmmo();
                     }
                 }
-            }
-
-            //StartCoroutine(CountdownShoot());
-            _ammoQuanity--;
-            _maxAmmoQuanity--;
-
-            if (_maxAmmoQuanity <= 0)
-            {
-                _ammoQuanity = _maxAmmo;
-                _maxAmmoQuanity = _maxAmmoCount;
-                _canShoot = true;
-                OnEmptyAmmo();
-            }
-        }
-        else
-        {
-            if (_ammoQuanity == 0 && _canShoot)
-            {
-                Reload();
             }
         }
     }
--- Assets/Scripts/PlayerAbilities/HeadHit.cs
+++ Assets/Scripts/PlayerAbilities/HeadHit.cs
@@ -1,14 +1,9 @@
-using Photon.Pun;
 using Photon.Realtime;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 public class HeadHit : HitDetector
 {
     public override void DetectHit(float damage, Player player)
     {
-        PlayerHealth.ApplyDamage(damage * 2, player);
-        Debug.Log("Голова");
+        PlayerHealth.ApplyDamage(damage * 1.5f, player);
     }
 }
