edce500351efebb0337597ef1c1fac46535d851a
Daehico <empire_95@mail.ru>
2022-10-28 16:25:46 +0300

    Change shotgun

--- Assets/Scripts/PlayerAbilities/Gun/Shotgun.cs
+++ Assets/Scripts/PlayerAbilities/Gun/Shotgun.cs
@@ -8,6 +8,7 @@ public class Shotgun : Gun
 {
     [SerializeField] private float _spread;
     [SerializeField] private int _coutOfShardsInOneBullet;
+    [SerializeField] private float _heightMultiplier;
 
     private float _fireaInterval;
 
@@ -49,11 +50,18 @@ public class Shotgun : Gun
             for (var y = 0; y < _coutOfShardsInOneBullet; y++)
             {
                 Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
-                Vector3 spreadVector = /*camera.transform.position +*/ (Random.insideUnitSphere * _spread);
-
+                Vector3 spreadVector = camera.transform.position + (Random.insideUnitSphere * _spread);
                 ray.origin = (camera.transform.position + spreadVector);
-                Debug.DrawRay(camera.transform.position, spreadVector,Color.green,100,true);
+
+
+                //float angle = y * (_spread / (_coutOfShardsInOneBullet - 1));
+                //Vector3 direction = Quaternion.AngleAxis(angle + (180 - (_spread / 2)), transform.up) * new Vector3(0, 0, -360);
+                //Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+                //Debug.DrawRay(camera.transform.position, spreadVector,Color.green,100,true);
                 RaycastHit[] hits = Physics.RaycastAll(ray, LayerToDetect);
+                //Physics physics = new Physics() ;
+                //RaycastHit[] hits = physics.ConeCastAll(transform.position, 3, transform.forward, 2, 3);
+                //RaycastHit[] hits = Physics.C
 
                 RaycastHit minDistanceHit = new RaycastHit
                 {
@@ -103,3 +111,33 @@ public class Shotgun : Gun
     }
 }
 
+
+//public static class ConeCastExtension
+//{
+//    public static RaycastHit[] ConeCastAll(this Physics physics, Vector3 origin, float maxRadius, Vector3 direction, float maxDistance, float coneAngle)
+//    {
+//        RaycastHit[] sphereCastHits = Physics.SphereCastAll(origin - new Vector3(0, 0, maxRadius), maxRadius, direction, maxDistance);
+//        List<RaycastHit> coneCastHitList = new List<RaycastHit>();
+
+//        //if (sphereCastHits.Length > 0)
+//        //{
+//        //    for (int i = 0; i < sphereCastHits.Length; i++)
+//        //    {
+//        //        sphereCastHits[i].collider.gameObject.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f);
+//        //        Vector3 hitPoint = sphereCastHits[i].point;
+//        //        Vector3 directionToHit = hitPoint - origin;
+//        //        float angleToHit = Vector3.Angle(direction, directionToHit);
+
+//        //        if (angleToHit < coneAngle)
+//        //        {
+//        //            coneCastHitList.Add(sphereCastHits[i]);
+//        //        }
+//        //    }
+//        //}
+
+//        RaycastHit[] coneCastHits = new RaycastHit[coneCastHitList.Count];
+//        coneCastHits = coneCastHitList.ToArray();
+
+//        return coneCastHits;
+//    }
+//}
