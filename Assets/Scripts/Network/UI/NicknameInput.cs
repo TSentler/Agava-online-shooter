454c4b667e35e4b688d8182bffd1eb8820043259
Shennod <Shennod1995@gmail.com>
2022-11-17 14:54:42 +0300

    add random nickname and fix menu scene

--- Assets/Scripts/Network/UI/NicknameInput.cs
+++ Assets/Scripts/Network/UI/NicknameInput.cs
@@ -1,4 +1,5 @@
 using Photon.Pun;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -7,18 +8,28 @@ namespace Network.UI
     [RequireComponent(typeof(TMP_InputField))]
     public class NicknameInput : MonoBehaviour
     {
+        [SerializeField] private List<string> _nickNames;
+
         private TMP_InputField _input;
 
         private void Awake()
         {
             _input = GetComponent<TMP_InputField>();
             _input.onValueChanged.AddListener(SetNick);
+            _input.text = GetRandomNickName();
             SetNick(_input.text);
         }
 
         private void SetNick(string value)
         {
             PhotonNetwork.NickName = value;
+        }
+
+        private string GetRandomNickName()
+        {
+            int nickNameIndex = Random.Range(0, _nickNames.Count);
+
+            return _nickNames[nickNameIndex];
         }
     }
 }
